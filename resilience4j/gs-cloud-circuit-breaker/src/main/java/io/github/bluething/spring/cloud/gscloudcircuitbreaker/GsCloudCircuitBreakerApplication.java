77f63aa7c2a452ac408fe0c62a0d608261e2c98d
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-24 10:56:09 +0700

    Init module

---
+++ resilience4j/gs-cloud-circuit-breaker/src/main/java/io/github/bluething/spring/cloud/gscloudcircuitbreaker/GsCloudCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.cloud.gscloudcircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsCloudCircuitBreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsCloudCircuitBreakerApplication.class, args);
+    }
+
+}
---
+++ resilience4j/gs-cloud-circuit-breaker/src/test/java/io/github/bluething/spring/cloud/gscloudcircuitbreaker/GsCloudCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.cloud.gscloudcircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsCloudCircuitBreakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
