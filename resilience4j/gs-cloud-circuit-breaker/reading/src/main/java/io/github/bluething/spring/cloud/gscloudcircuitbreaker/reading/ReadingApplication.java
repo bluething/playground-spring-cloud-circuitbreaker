74832fe373db7b0bb9203940e4ddd51debac63e2
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-24 11:23:59 +0700

    Init reading

---
+++ resilience4j/gs-cloud-circuit-breaker/reading/src/main/java/io/github/bluething/spring/cloud/gscloudcircuitbreaker/reading/ReadingApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.cloud.gscloudcircuitbreaker.reading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingApplication.class, args);
+    }
+
+}
---
+++ resilience4j/gs-cloud-circuit-breaker/reading/src/test/java/io/github/bluething/spring/cloud/gscloudcircuitbreaker/reading/ReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.cloud.gscloudcircuitbreaker.reading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
