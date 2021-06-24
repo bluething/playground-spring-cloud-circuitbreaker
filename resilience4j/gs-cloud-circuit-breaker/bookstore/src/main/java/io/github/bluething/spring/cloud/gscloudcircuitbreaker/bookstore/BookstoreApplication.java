f847f4b0efd2cc509f7d54cd5855a6da109e22c4
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-06-24 11:26:07 +0700

    Init bookstore

---
+++ resilience4j/gs-cloud-circuit-breaker/bookstore/src/main/java/io/github/bluething/spring/cloud/gscloudcircuitbreaker/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.cloud.gscloudcircuitbreaker.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
---
+++ resilience4j/gs-cloud-circuit-breaker/bookstore/src/test/java/io/github/bluething/spring/cloud/gscloudcircuitbreaker/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.spring.cloud.gscloudcircuitbreaker.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
